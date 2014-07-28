fbd8a2c5fd44b06499ec2b460d21f45bd1f9f0bb
= <=>
2014-07-28 18:22:12 +0800

    change name

--- src/com/example/textrotator/SphereView.java
+++ src/com/example/textrotator/SphereView.java
@@ -22,28 +22,28 @@ public class SphereView extends View {
 	private Matrix mMatrix = new Matrix();
 	private Paint mPaint = new Paint();
 
-	String[] text = { "水煮鱼", "红烧肉", "烧排骨", "瓜丝儿", "拌海蜇", "炝冬笋", "玉兰片", "烧鱼头",
+	private String[] mText = { "水煮鱼", "红烧肉", "烧排骨", "瓜丝儿", "拌海蜇", "炝冬笋", "玉兰片", "烧鱼头",
 			"炸面筋", "炸豆腐" };
-	int size = 10;
-	int TextSize = 40;
-	float[] random = new float[size];
+	private int mSize = 10;
+	private int mTextSize = 40;
+	private float[] mRandom = new float[mSize];
 
 	// [0]:旋转的角度，[1]:X坐标，[2]:Y坐标
-	float[][] textPosition = new float[size][3];
+	private float[][] mTextPosition = new float[mSize][3];
 
 	public SphereView(Context context, AttributeSet attributeSet) {
 		super(context, attributeSet);
 		setWillNotDraw(false);
 		mCamera = new Camera();
 		mPaint.setAntiAlias(true);
-		mPaint.setTextSize(TextSize);
+		mPaint.setTextSize(mTextSize);
 		createRandom();
 	}
 
 	// 创造一组-50到+50的随机数,作为旋转的角度
 	private void createRandom() {
-		for (int i = 0; i < size; i++) {
-			random[i] = (float) (Math.random() * 100 - 50);
+		for (int i = 0; i < mSize; i++) {
+			mRandom[i] = (float) (Math.random() * 100 - 50);
 		}
 	}
 
@@ -51,13 +51,13 @@ public class SphereView extends View {
 	public void draw(Canvas canvas) {
 
 		float time = 0;
-		for (int i = 0; i < size; i++, time++) {
+		for (int i = 0; i < mSize; i++, time++) {
 
 			float speed = (i + 1) / 5f;
-			rotate2(random[i], time, 1, i);
+			rotate2(mRandom[i], time, 1, i);
 			canvas.save();
 			canvas.setMatrix(mMatrix);
-			canvas.drawText(text[i], 0, 0, mPaint);
+			canvas.drawText(mText[i], 0, 0, mPaint);
 			canvas.restore();
 			invalidate();
 		}
@@ -94,33 +94,33 @@ public class SphereView extends View {
 		mMatrix.getValues(fMatrix);
 		
 		// 拿到角度和XY的值
-		textPosition[textNum][0] = degreeY % 360;
-		textPosition[textNum][1] = fMatrix[2];
-		textPosition[textNum][2] = fMatrix[5];
+		mTextPosition[textNum][0] = degreeY % 360;
+		mTextPosition[textNum][1] = fMatrix[2];
+		mTextPosition[textNum][2] = fMatrix[5];
 
 	}
 
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
 		int x = (int) event.getX();
-		int y = (int) event.getY() + TextSize;
+		int y = (int) event.getY() + mTextSize;
 
 		if (event.getAction() == MotionEvent.ACTION_DOWN) {
 
-			for (int i = 0; i < size; i++)
-				if (textPosition[i][0] < 90 || textPosition[i][0] > 270) {
-					if (textPosition[i][1] > x - 50
-							&& textPosition[i][1] < x + 50
-							&& textPosition[i][2] > y - 30
-							&& textPosition[i][2] < y + 30)
-						Toast.makeText(getContext(), text[i],
+			for (int i = 0; i < mSize; i++)
+				if (mTextPosition[i][0] < 90 || mTextPosition[i][0] > 270) {
+					if (mTextPosition[i][1] > x - 50
+							&& mTextPosition[i][1] < x + 50
+							&& mTextPosition[i][2] > y - 30
+							&& mTextPosition[i][2] < y + 30)
+						Toast.makeText(getContext(), mText[i],
 								Toast.LENGTH_SHORT).show();
 				}
 
 		}
 		Log.i("My", "x:" + x + "y:" + y);
-		Log.i("My", "度数" + textPosition[0][0] + "!X:" + textPosition[0][1]
-				+ "!Y:" + textPosition[0][2]);
+		Log.i("My", "度数" + mTextPosition[0][0] + "!X:" + mTextPosition[0][1]
+				+ "!Y:" + mTextPosition[0][2]);
 
 		return true;
 	}
